d0c841f93b4bbb84aab05ff82f56dccea9db7569
qw321 <atharkhan188@gmail.com>
2026-07-06 14:55:33 +0530

    Add Scroll-to-Top button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
-// CRUSTAR Version 2
+// ================= SCROLL TO TOP =================
 
-console.log("CRUSTAR Website Loaded");
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function () {
+
+    if (document.documentElement.scrollTop > 300) {
+
+        scrollBtn.style.display = "block";
+
+    } else {
+
+        scrollBtn.style.display = "none";
+
+    }
+
+};
+
+scrollBtn.onclick = function () {
+
+    window.scrollTo({
+
+        top: 0,
+
+        behavior: "smooth"
+
+    });
+
+};
